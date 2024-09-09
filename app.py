a2dea79ffd244a5042f12ce11ad5116ba4cbc7b5
Komal Patra <154806647+Komal-patra@users.noreply.github.com>
2024-09-09 22:01:53 +0100

    Final check version for deploying

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def predictRoute():
 if __name__ == "__main__":
     clApp = ClientApp()
     # edited the aws secret
-    app.run(host='0.0.0.0', port=80) #for AWS
+    app.run(host='0.0.0.0', port=8080) #for AWS
